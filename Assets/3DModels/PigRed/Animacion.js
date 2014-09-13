f76f7c5a65b638b4dc6f3cef35d560adf6108e07
pa3lo <sig_pa3lo@yahoo.com>
2014-09-13 18:05:14 -0300

    Creacion del proyecto
    Se crea el proyecto con el chancho rojo animado.

---
+++ Assets/3DModels/PigRed/Animacion.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+var anim : Animator;
+
+function Start () {
+
+anim = GetComponent(Animator);
+
+}
+
+function Update () {
+	
+	
+//	anim.SetBool("caminar", false);
+	print("caminar: " + anim.GetBool("caminar"));
+	
+	if (Input.touchCount > 0)
+		{
+		for (var i = 0; i < Input.touchCount; ++i)
+			{
+			if (Input.GetTouch(i).phase == TouchPhase.Began)
+				{
+				anim.SetBool("caminar", true);
+				}
+		
+			if (Input.GetTouch(i).phase == TouchPhase.Ended)
+				{
+				anim.SetBool("caminar", false);
+				}
+			}
+		}
+ }
